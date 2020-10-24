2030eb4cb0a8da0e2a1f089c354e78392d035c38
Jimmy Yeh <chienfeng0719@gmail.com>
2020-10-24 17:06:34 +0800

    fix drop 備註 column bug

--- tw_stock_plugin/update_stock.py
+++ tw_stock_plugin/update_stock.py
@@ -21,6 +21,8 @@ import requests
 import pandas as pd
 from time import sleep
 
+from tw_stock_plugin.config import Config
+
 
 class UpdateStock:
     _URL_DICT = {
@@ -46,22 +48,22 @@ class UpdateStock:
         :return:
         """
         # create csv dir if dir not exists
-        if not os.path.isdir('./csv'):
-            os.mkdir('./csv')
+        if not os.path.isdir(Config.STOCK_CSV_DIR):
+            os.mkdir(Config.STOCK_CSV_DIR)
 
         print('UPDATING STOCK INFO')
         for category, url in cls._URL_DICT.items():
             response = cls._get_response(url=url)
             df = pd.read_html(response.text)[0]
-            df = df.dropna(axis=1)
             df_columns = df.iloc[0]
             new_data = [data for data in df.values.tolist() if cls._CODE_NAME_PATTERN.search(data[0])]
             df = pd.DataFrame(new_data, columns=df_columns)
             code_name_df = pd.DataFrame(df['有價證券代號及名稱'].str.split('\u3000', 1).tolist(),
                                         columns=['證券代號', '證券名稱'])
-            df = df.drop('有價證券代號及名稱', axis=1)
+            df = df.drop(['有價證券代號及名稱', '備註'], axis=1)
             df = pd.concat([code_name_df, df], axis=1)
-            df.to_csv(f'./csv/{category}.csv')
+            csv_path = os.path.join(Config.STOCK_CSV_DIR, f'{category}.csv')
+            df.to_csv(csv_path)
             # to avoiding banned by twse website
             sleep(3)
         print('FINISHED')
